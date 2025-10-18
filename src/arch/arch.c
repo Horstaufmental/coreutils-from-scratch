c99ec096848d3760f2361272f50b4254363e6760
Horstaufmental <182580623+Horstaufmental@users.noreply.github.com>
2025-10-18 23:59:12 +0700

    arch - sys/utsname.h instead of manual implementation

--- src/arch/arch.c
+++ src/arch/arch.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <sys/utsname.h>
 
 struct help_entry {
   const char *opt;
@@ -30,23 +31,6 @@ void print_help(const char *name) {
   }
 }
 
-// yeah im redefining sys/utsname.h
-struct utsname {
-  char sysname[65];  /* Operating system name (e.g., "Linux") */
-  char nodename[65]; /* Name within communications network
-                        to which the node is attached, if any */
-  char release[65];  /* Operating system release
-                        (e.g., "2.6.28") */
-  char version[65];  /* Operating system version */
-  char machine[65];  /* Hardware type identifier */
-};
-
-int uname(struct utsname *buf) {
-  if (syscall(63, buf) != 0)
-    return -1;
-  return 0;
-}
-
 int main(int argc __attribute__((unused)), char *argv[]) {
   struct utsname sys_info;
 
@@ -55,22 +39,16 @@ int main(int argc __attribute__((unused)), char *argv[]) {
       print_help(argv[0]);
       return 0;
     } else {
-      char buffer[257];
-      sprintf(buffer, "%s: unrecognized option '%s'\nTry '%s --help' for more information", argv[0], argv[1], argv[0]);
-      write(1, buffer, strlen(buffer));
+      fprintf(stderr, "%s: unrecognized option '%s'\nTry '%s --help' for more information", argv[0], argv[1], argv[0]);
       return 1;
     }
   }
 
   if (uname(&sys_info) == -1) {
-    char buffer[257] = "rm: cannot retrieve system info: ";
-    strncat(buffer, strerror(errno), sizeof(buffer) - strlen(buffer) - strlen(strerror(errno))); strncat(buffer, "\n", sizeof(buffer) - strlen(buffer) - 1);
-    write(2, buffer, strlen(buffer));
+    fprintf(stderr, "rm: cannot retrieve system info: %s\n", strerror(errno));
     return 1;
   }
 
-  char buffer[257];
-  sprintf(buffer, "%s\n", sys_info.machine);
-  write(1, buffer, strlen(buffer));
+  puts(sys_info.machine);
   return 0;
 }
