22ca5a56ffb1bb07efacfcacbafd88f949b4a8b9
Horstaufmental <182580623+Horstaufmental@users.noreply.github.com>
2025-12-06 04:24:53 +0700

    cat - mmap

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -14,12 +14,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  */
+#define _GNU_SOURCE
+
+#include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 // oh welp js realized this exists, been manually
 // defining bool in like the previous 928469 codes (sarcasm)
 #include <errno.h>
@@ -28,11 +33,12 @@
 #define PROGRAM_NAME "cat"
 #define PROJECT_NAME "coreutils from scratch"
 #define AUTHORS "Horstaufmental"
-#define VERSION "1.1 (Okami Era)"
+#define VERSION "1.2"
 
 #define BUFSIZE 32768 // GNU Coreutils's buffer size for files
 
-struct help_entry {
+struct help_entry
+{
   const char *opt;
   const char *desc;
 };
@@ -65,10 +71,10 @@ static struct help_entry help_entries[] = {
     {"-v, --show-nonprinting", "use ^ and M- notation, except for LFD and TAB"},
     {"    --help", "display this help and exit"},
     {"    --version", "output version information and exit"},
-    {NULL, NULL}
-};
+    {NULL, NULL}};
 
-void print_help(const char *name) {
+void print_help(const char *name)
+{
   printf("Usage: %s [OPTION]... [FILE]...\n", name);
   printf("Concatenate FILE(s) to standard output.\n\n");
 
@@ -76,14 +82,16 @@ void print_help(const char *name) {
 
   // find longest option string
   int maxlen = 0;
-  for (int i = 0; help_entries[i].opt; i++) {
+  for (int i = 0; help_entries[i].opt; i++)
+  {
     int len = (int)strlen(help_entries[i].opt);
     if (len > maxlen)
       maxlen = len;
   }
 
   // print each option aligned
-  for (int i = 0; help_entries[i].opt; i++) {
+  for (int i = 0; help_entries[i].opt; i++)
+  {
     printf("  %-*s  %s\n", maxlen, help_entries[i].opt, help_entries[i].desc);
   }
   fputs("\nExamples:\n"
@@ -93,47 +101,221 @@ void print_help(const char *name) {
         stdout);
 }
 
-void print_version() {
+void print_version()
+{
   printf("%s (%s) %s\n", PROGRAM_NAME, PROJECT_NAME, VERSION);
   printf("Copyright (C) 2025 %s\n", AUTHORS);
   puts("License GPLv3+: GNU GPL version 3 or later "
-  "<https://gnu.org/licenses/gpl.html>.\n"
-  "This is free software: you are free to change and redistribute it.\n"
-  "There is NO WARRANTY, to the extent permitted by law.\n");
+       "<https://gnu.org/licenses/gpl.html>.\n"
+       "This is free software: you are free to change and redistribute it.\n"
+       "There is NO WARRANTY, to the extent permitted by law.\n");
   printf("Written by %s\n", AUTHORS);
 }
 
 static unsigned long long line_number = 1;
 
-void printLineNum(bool isBlank, bool numberAll, bool numberNonBlank) {
+void printLineNum(bool isBlank, bool numberAll, bool numberNonBlank)
+{
   if (numberNonBlank && isBlank)
     return;
-  if (numberAll || (numberNonBlank && !isBlank)) {
+  if (numberAll || (numberNonBlank && !isBlank))
+  {
     printf("%6llu\t", line_number++);
   }
 }
 
-void printVis(unsigned char c, bool showNonPrinting) {
-  if (!showNonPrinting) {
+void printVis(unsigned char c, bool showNonPrinting)
+{
+  if (!showNonPrinting)
+  {
     putchar(c);
     return;
   }
 
-  if (c == '\n' || c == '\t') {
+  if (c == '\n' || c == '\t')
+  {
     putchar(c);
-  } else if (c < 32) {
+  }
+  else if (c < 32)
+  {
     printf("^%c", c + 64); // %@ through ^_
-  } else if (c == 127) {
+  }
+  else if (c == 127)
+  {
     fputs("^?", stdout);
-  } else if (c >= 128) {
+  }
+  else if (c >= 128)
+  {
     fputs("M-", stdout);
     printVis(c - 128, true);
-  } else {
+  }
+  else
+  {
     putchar(c);
   }
 }
 
-int main(int argc, char *argv[]) {
+int read_fd(int fd, bool showNonPrinting, bool showTabs,
+            bool squeezeBlank, bool outputNumber, bool showEnds,
+            bool numberNoBlank)
+{
+  char buf[BUFSIZE];
+  ssize_t n;
+  bool atLineStart = true;
+  bool prevBlank = false;
+
+  while ((n = read(fd, buf, sizeof(buf))) > 0)
+  {
+    for (ssize_t i = 0; i < n; i++)
+    {
+      unsigned char c = buf[i];
+
+      if (c == '\n')
+      {
+        if (prevBlank && squeezeBlank)
+        {
+          // skip
+          continue;
+        }
+
+        if (atLineStart)
+        {
+          printLineNum(true, outputNumber, numberNoBlank);
+        }
+
+        if (showEnds)
+          putchar('$');
+        putchar('\n');
+
+        prevBlank = true;
+        atLineStart = true;
+      }
+      else
+      {
+        if (atLineStart)
+        {
+          printLineNum(false, outputNumber, numberNoBlank);
+          atLineStart = false;
+        }
+        prevBlank = false;
+
+        // handle -T and -v
+        if (c == '\t' && showTabs)
+        {
+          fputs("^I", stdout);
+        }
+        else
+        {
+          printVis(c, showNonPrinting);
+        }
+      }
+    }
+  }
+
+  if (n == -1)
+  {
+    fprintf(stderr, "cat: %s\n", strerror(errno));
+    return 1;
+  }
+  return 0;
+}
+
+int read_fd_mmap(int fd, size_t file_size, bool showNonPrinting, bool showTabs,
+                 bool squeezeBlank, bool outputNumber, bool showEnds,
+                 bool numberNoBlank)
+{
+  void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (data == MAP_FAILED)
+    return read_fd(fd, showNonPrinting, showTabs,
+                   squeezeBlank, outputNumber, showEnds,
+                   numberNoBlank);
+
+  madvise(data, file_size, MADV_SEQUENTIAL);
+  unsigned char *buf = (unsigned char *)data;
+  bool atLineStart = true;
+  bool prevBlank = false;
+
+  for (size_t i = 0; i < file_size; i++)
+  {
+    unsigned char c = buf[i];
+
+    if (c == '\n')
+    {
+      if (prevBlank && squeezeBlank)
+      {
+        // skip
+        continue;
+      }
+
+      if (atLineStart)
+      {
+        printLineNum(true, outputNumber, numberNoBlank);
+      }
+
+      if (showEnds)
+        putchar('$');
+      putchar('\n');
+
+      prevBlank = true;
+      atLineStart = true;
+    }
+    else
+    {
+      if (atLineStart)
+      {
+        printLineNum(false, outputNumber, numberNoBlank);
+        atLineStart = false;
+      }
+      prevBlank = false;
+
+      // handle -T and -v
+      if (c == '\t' && showTabs)
+      {
+        fputs("^I", stdout);
+      }
+      else
+      {
+        printVis(c, showNonPrinting);
+      }
+    }
+  }
+  munmap(data, file_size);
+  return 0;
+}
+
+int read_wrapper(int fd, bool showNonPrinting, bool showTabs,
+                 bool squeezeBlank, bool outputNumber, bool showEnds,
+                 bool numberNoBlank)
+{
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+  {
+    fprintf(stderr, "cat: %s\n", strerror(errno));
+    return read_fd(fd, showNonPrinting, showTabs,
+                   squeezeBlank, outputNumber, showEnds,
+                   numberNoBlank);
+  }
+
+  if (S_ISREG(st.st_mode) && st.st_size > 65536)
+  {
+    puts("Using mmap");
+    sleep(1);
+    return read_fd_mmap(fd, st.st_size, showNonPrinting, showTabs,
+                        squeezeBlank, outputNumber, showEnds,
+                        numberNoBlank);
+  }
+  else
+  {
+    puts("Using read");
+    sleep(1);
+    return read_fd(fd, showNonPrinting, showTabs,
+                   squeezeBlank, outputNumber, showEnds,
+                   numberNoBlank);
+  }
+}
+
+int main(int argc, char *argv[])
+{
   int opt;
 
   bool showNonPrinting = false; // -v
@@ -143,8 +325,10 @@ int main(int argc, char *argv[]) {
   bool showEnds = false;        // -E
   bool numberNoBlank = false;   // -b
 
-  while ((opt = getopt_long(argc, argv, "AbeEnstTuv", long_options, 0)) != -1) {
-    switch (opt) {
+  while ((opt = getopt_long(argc, argv, "AbeEnstTuv", long_options, 0)) != -1)
+  {
+    switch (opt)
+    {
     case 'A':
       showNonPrinting = showEnds = showTabs = true;
       break;
@@ -184,19 +368,26 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (argc - optind == 0) {
+  if (argc - optind == 0)
+  {
     char buf[4096];
-    while (true) {
+    while (true)
+    {
       fgets(buf, sizeof(buf), stdin);
       if (feof(stdin))
         break;
       write(STDOUT_FILENO, buf, strlen(buf));
     }
-  } else {
-    for (; optind < argc; optind++) {
-      if (strcmp(argv[optind], "-") == 0) {
+  }
+  else
+  {
+    for (; optind < argc; optind++)
+    {
+      if (strcmp(argv[optind], "-") == 0)
+      {
         char buf[BUFSIZ];
-        while (true) {
+        while (true)
+        {
           fgets(buf, sizeof(buf), stdin);
           if (feof(stdin))
             break;
@@ -205,53 +396,23 @@ int main(int argc, char *argv[]) {
         continue;
       }
       int fd = open(argv[optind], O_RDONLY);
-      if (fd == -1) {
+      if (fd == -1)
+      {
         fprintf(stderr, "cat: cannot open '%s': %s\n", argv[optind],
                 strerror(errno));
         return 1;
       }
 
-      char buf[BUFSIZE];
-      ssize_t n;
-      bool atLineStart = true;
-      bool prevBlank = false;
-
-      while ((n = read(fd, buf, sizeof(buf))) > 0) {
-        for (ssize_t i = 0; i < n; i++) {
-          unsigned char c = buf[i];
-
-          if (c == '\n') {
-            if (prevBlank && squeezeBlank) {
-              // skip
-              continue;
-            }
-
-            if (atLineStart) {
-              printLineNum(true, outputNumber, numberNoBlank);
-            }
-
-            if (showEnds)
-              putchar('$');
-            putchar('\n');
-
-            prevBlank = true;
-            atLineStart = true;
-          } else {
-            if (atLineStart) {
-              printLineNum(false, outputNumber, numberNoBlank);
-              atLineStart = false;
-            }
-            prevBlank = false;
-
-            // handle -T and -v
-            if (c == '\t' && showTabs) {
-              fputs("^I", stdout);
-            } else {
-              printVis(c, showNonPrinting);
-            }
-          }
-        }
+      if (read_wrapper(fd, showNonPrinting, showTabs,
+                       squeezeBlank, outputNumber, showEnds,
+                       numberNoBlank) != 0)
+      {
+        fprintf(stderr, "cat: '%s': %s\n", argv[optind],
+                strerror(errno));
+        close(fd);
+        return 1;
       }
+
       close(fd);
     }
   }
