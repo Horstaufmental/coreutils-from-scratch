0ac8917f60ac99b2b9e25588c7c004a223330777
Horstaufmental <182580623+Horstaufmental@users.noreply.github.com>
2025-12-06 03:30:12 +0700

    wc final

--- src/wc/maxlen_test.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-
-./random_string
-./wc -L ./gibberish-utf8-ascii-mix.txt
-wc -L ./gibberish-utf8-ascii-mix.txt
--- src/wc/wc.c
+++ src/wc/wc.c
@@ -1,3 +1,19 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This file is part of coreutils from scratch.
+ * Copyright (c) 2025 Horstaufmental
+ *
+ * coreutils from scratch is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * coreutils from scratch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ */
 #define _GNU_SOURCE
 
 #include <errno.h>
@@ -28,10 +44,72 @@
 #define T_ONLY (1 << 2)
 #define T_NEVR (1 << 3)
 
+#define PROGRAM_NAME "wc"
+#define PROJECT_NAME "coreutils from scratch"
+#define AUTHORS "Horstaufmental"
+#define VERSION "1.0"
+
 struct help_entry {
   const char *opt;
   const char *desc;
 };
+
+static struct help_entry help_entries[] = {
+  {"-c, --bytes", "print the byte counts"},
+  {"-m, --chars", "print the character counts"},
+  {"-l, --lines", "print the newline counts"},
+  {"    --files0-from=F", "read input from the files specified by\n"
+   "                      NUL-terminated names in file F;\n"
+   "                      if F is - then read names from standard input"},
+  {"-L, --max-line-length", "print the maximum display width"},
+  {"-w, --words", "print the word counts"},
+  {"    --total=WHEN", "when to print a line with total counts;\n"
+   "                      WHEN can be: auto, always, only, never"},
+  {"    --help", "display this help and exit"},
+  {"    --version", "output version information and exit"},
+  {0, 0}
+};
+
+void print_help(const char *name)
+{
+  printf("Usage: %s [OPTION]... [FILE]...\n"
+         "  or:  %s [OPTION]... --files0-from=F\n",
+         name, name);
+  puts("Print newline, word, and byte counts for each FILE, and a total line if.\n"
+       "more than one FILE is specified.  A word is a nonempty sequence of non white\n"
+       "space delimited by white space characters or by start or end of input.\n\n"
+
+       "With no FILE, or when FILE is -, read standard input.\n\n"
+
+       "The options below may be used to select which counts are printed, always in\n"
+       "the following order: newline, word, character, byte, maximum line length.\n");
+
+  // find longest option string
+  int maxlen = 0;
+  for (int i = 0; help_entries[i].opt; i++)
+  {
+    int len = (int)strlen(help_entries[i].opt);
+    if (len > maxlen)
+      maxlen = len;
+  }
+
+  // print each option aligned
+  for (int i = 0; help_entries[i].opt; i++)
+  {
+    printf("  %-*s  %s\n", maxlen, help_entries[i].opt, help_entries[i].desc);
+  }
+}
+
+void print_version()
+{
+  printf("%s (%s) %s\n", PROGRAM_NAME, PROJECT_NAME, VERSION);
+  printf("Copyright (C) 2025 %s\n", AUTHORS);
+  puts("License GPLv3+: GNU GPL version 3 or later "
+       "<https://gnu.org/licenses/gpl.html>.\n"
+       "This is free software: you are free to change and redistribute it.\n"
+       "There is NO WARRANTY, to the extent permitted by law.\n");
+  printf("Written by %s\n", AUTHORS);
+}
 
 static struct option long_options[] = {{"bytes", no_argument, 0, 'c'},
                                        {"chars", no_argument, 0, 'm'},
@@ -53,7 +131,7 @@ Do NOT use print_to_var() for input. Ever.
 
 - ChatGPT, 2025
 
-it's weird and quirky i know, and im keeping this as a relic of my stupidity
+it's weird and quirky i know, and im keeping this as a relic
 
 void print_to_var(char *buf, char *str, bool comma) {
   char buffer[4096];
@@ -93,8 +171,8 @@ static inline bool is_word_seperator(wchar_t wc) {
   }
 }
 
-// i am declaring that i wrote the maxlen part correctly and the GNU people
-// didnt (~66 in a 75 million long file is crazy tho)
+// i am declaring that i wrote the maxlen part correctly and the GNU people didnt!!
+// (~66 diff in a 75 million long file is crazy tho)
 struct wc count_word_fd(int fd) {
   const size_t BUF_SZ = 524288;
   unsigned char *buf = malloc(BUF_SZ);
@@ -316,68 +394,99 @@ struct wc cw_wrapper(int fd) {
   }
 }
 
+int num_width(long x) {
+  int width = 0;
+  if (x <= 0) {
+    width = 1;
+    x = -x;
+  }
+  while (x > 0) {
+    width++;
+    x /= 10;
+  }
+  return width;
+}
+
 // why do wc from stdin and file formats differently wtf!!
 void print_results(uint8_t flags, char *name, struct wc willer,
                    bool from_stdin) {
   bool first_print = true;
 
+  int w1 = num_width(willer.lines);
+  int w2 = num_width(willer.words);
+  int w3 = num_width(willer.chars);
+  int w4 = num_width(willer.bytes);
+  int w5 = num_width(willer.maxlen);
+
+  int width = 0;
+  if (w1 > width && flags & P_LINES)
+    width = w1;
+  if (w2 > width && flags & P_WORDS)
+    width = w2;
+  if (w3 > width && flags & P_CHARS)
+    width = w3;
+  if (w4 > width && flags & P_BYTES)
+    width = w4;
+  if (w5 > width && flags & P_LENMX)
+    width = w5;
+
   if (flags & P_DEFAULT) {
-    printf("%6ld %7ld %7ld", willer.lines, willer.words, willer.bytes);
+    printf("%7ld %7ld %7ld", willer.lines, willer.words, willer.bytes);
   }
   if (flags & P_LINES) {
     if (!from_stdin)
       if (!first_print) {
-        printf("%ld", willer.lines);
+        printf("%*ld", width, willer.lines);
         first_print = false;
       } else {
-        printf(" %ld", willer.lines);
+        printf(" %*ld", width, willer.lines);
       }
     else
-      printf("%ld ", willer.lines);
+      printf("%7ld ", willer.lines);
   }
   if (flags & P_WORDS) {
     if (!from_stdin)
       if (!first_print) {
-        printf("%ld", willer.words);
+        printf("%*ld", width, willer.words);
         first_print = false;
       } else {
-        printf(" %ld", willer.words);
+        printf(" %*ld", width, willer.words);
       }
     else
-      printf("%6ld ", willer.words);
+      printf("%7ld ", willer.words);
   }
   if (flags & P_CHARS) {
     if (!from_stdin)
       if (!first_print) {
-        printf("%ld", willer.chars);
+        printf("%*ld", width, willer.chars);
         first_print = false;
       } else {
-        printf(" %ld", willer.chars);
+        printf(" %*ld", width, willer.chars);
       }
     else
-      printf("%6ld ", willer.chars);
+      printf("%7ld ", willer.chars);
   }
   if (flags & P_BYTES) {
     if (!from_stdin)
       if (!first_print) {
-        printf("%ld", willer.bytes);
+        printf("%*ld", width, willer.bytes);
         first_print = false;
       } else {
-        printf(" %ld", willer.bytes);
+        printf(" %*ld", width, willer.bytes);
       }
     else
-      printf("%6ld ", willer.bytes);
+      printf("%7ld ", willer.bytes);
   }
   if (flags & P_LENMX) {
     if (!from_stdin)
       if (!first_print) {
-        printf("%ld", willer.maxlen);
+        printf("%*ld", width, willer.maxlen);
         first_print = false;
       } else {
-        printf(" %ld", willer.maxlen);
+        printf(" %*ld", width, willer.maxlen);
       }
     else
-      printf("%6ld ", willer.maxlen);
+      printf("%7ld ", willer.maxlen);
   }
 
   if (!from_stdin && name != NULL && name[0] != '\0') {
@@ -462,8 +571,10 @@ int main(int argc, char *argv[]) {
       }
       break;
     case 1:
+      print_help(argv[0]);
       return 0;
     case 2:
+      print_version();
       return 0;
     case '?':
       fprintf(stderr, "Try '%s --help' for more information\n", argv[0]);
@@ -491,14 +602,16 @@ int main(int argc, char *argv[]) {
     size_t name_count = 0;
 
     if (files0_from_stdin) {
-      while ((bytes = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+      while ((bytes = read(STDIN_FILENO, buf + buf_used, sizeof(buf) - buf_used)) > 0) {
         buf_used += bytes;
         size_t start = 0;
         for (size_t i = 0; i < buf_used; i++) {
           if (buf[i] == '\0') {
-            list[name_count] = strdup(&buf[start]);
-            name_count++;
-
+            size_t len = i - start;
+            if (len > 0) {
+              list[name_count] = strndup(&buf[start], len);
+              name_count++;
+            }
             start = i + 1;
           }
         }
@@ -508,22 +621,20 @@ int main(int argc, char *argv[]) {
           buf_used -= start;
         } else {
           buf_used = 0;
-        }
-
-        if (bytes == -1) {
-          fprintf(stderr, "wc: %s\n", strerror(errno));
-          return 1;
         }
       }
     } else {
-      while ((bytes = read(files0_from_fd, buf, sizeof(buf))) > 0) {
+      while ((bytes = read(files0_from_fd, buf + buf_used, sizeof(buf) - buf_used)) > 0) {
         buf_used += bytes;
         size_t start = 0;
         for (size_t i = 0; i < buf_used; i++) {
           if (buf[i] == '\0') {
-            list[name_count] = strdup(&buf[start]);
-            name_count++;
-
+            size_t len = i - start;
+            if (len > 0)
+            {
+              list[name_count] = strndup(&buf[start], len);
+              name_count++;
+            }
             start = i + 1;
           }
         }
@@ -534,27 +645,36 @@ int main(int argc, char *argv[]) {
         } else {
           buf_used = 0;
         }
-
-        if (bytes == -1) {
-          fprintf(stderr, "wc: %s\n", strerror(errno));
-          close(files0_from_fd);
-          return 1;
-        }
       }
     }
+
+    if (buf_used > 0) {
+      // handle last entry if not null-terminated
+      list[name_count] = strndup(buf, buf_used);
+      name_count++;
+    }
+
+    if (bytes == -1)
+    {
+      fprintf(stderr, "wc: %s\n", strerror(errno));
+      close(files0_from_fd);
+      return 1;
+    }
+
     if (!files0_from_stdin)
       close(files0_from_fd);
 
-    for (size_t i = 0; i < sizeof(list) / sizeof(list[0]); i++) {
+    for (size_t i = 0; i < name_count; i++) {
       int fd = open(list[i], O_RDONLY | O_CLOEXEC);
       if (fd == -1) {
         fprintf(stderr, "%s: %s: %s\n", argv[0], list[i], strerror(errno));
-        close(fd);
         return 1;
       }
+      strncpy(names[count], list[i], sizeof(names[count]));
       process_the_fucking_struct(
           fd, williams, sizeof(williams) / sizeof(williams[0]), &count);
       close(fd);
+      free(list[i]);// free the strdup'd memory
     }
   } else if (argc == optind) {
   do_stdin:;
@@ -606,23 +726,41 @@ int main(int argc, char *argv[]) {
     char totalbuf[256];
     size_t pos = 0;
 
+    int w1 = num_width(final.lines);
+    int w2 = num_width(final.words);
+    int w3 = num_width(final.chars);
+    int w4 = num_width(final.bytes);
+    int w5 = num_width(final.maxlen);
+
+    int width = 0;
+    if (w1 > width && flags & P_LINES)
+      width = w1;
+    if (w2 > width && flags & P_WORDS)
+      width = w2;
+    if (w3 > width && flags & P_CHARS)
+      width = w3;
+    if (w4 > width && flags & P_BYTES)
+      width = w4;
+    if (w5 > width && flags & P_LENMX)
+      width = w5;
+
     if (flags & P_LINES)
       pos +=
-          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld ", final.lines);
+          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%*ld ", width, final.lines);
     if (flags & P_WORDS)
       pos +=
-          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld ", final.words);
+          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%*ld ", width, final.words);
     if (flags & P_CHARS)
       pos +=
-          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld ", final.chars);
+          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%*ld ", width, final.chars);
     if (flags & P_BYTES)
       pos +=
-          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld ", final.bytes);
+          snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%*ld ", width, final.bytes);
     if (flags & P_LENMX)
-      pos += snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld ",
+      pos += snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%*ld ", width,
                       final.maxlen);
     if (flags & P_DEFAULT)
-      pos += snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%ld %ld %ld",
+      pos += snprintf(totalbuf + pos, sizeof(totalbuf) - pos, "%7ld %7ld %7ld",
                       final.lines, final.words, final.bytes);
 
     totalbuf[pos] = 0;
